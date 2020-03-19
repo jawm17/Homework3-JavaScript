e3cbb16e2df6df2ae48e35eba7b2f592eab68ed0
Michael Sanderson <jawm17@gmail.com>
2020-03-18 21:38:05 -0500

    created prompts and checker

--- script.js
+++ script.js
@@ -5,10 +5,28 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+
+function generatePassword() {
+    var numCharacters = numCharactersPrompt();
+    var specialCharacters = confirm("Click OK to include special characters");
+    var numericCharacters = confirm("Click OK to include numeric characters");
+    var lowerCase = confirm("Click OK to include lowercase characters");
+    var upperCase = confirm("Click OK to include uppercase characters");
+}
+
+function numCharactersPrompt() {
+  var numCharactersRaw = prompt("How many characters would you like your password to contain?");
+  if (!isNaN(numCharactersRaw) && numCharactersRaw >= 8 && numCharactersRaw <= 128) {
+    return numCharactersRaw;
+  }
+  else {
+    alert("You must input a valid integer of at least 8 and no more than 128");
+    numCharactersPrompt();
+  }
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
