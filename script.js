cce57255d001582dda79cd180ad8a3a362e7ee10
Michael Sanderson <jawm17@gmail.com>
2020-03-19 19:47:10 -0500

    condensed code/more efficient

--- script.js
+++ script.js
@@ -1,79 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var special = ["!",",","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
-var specialCharacters = false;
-var numericCharacters = false;
-var lowerCase = false;
-var upperCase = false;
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var bigAlphabet = alphabet.map(letters => letters.toUpperCase());
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  document.querySelector("#password").value = password;
 }
 
 function generatePassword() {
-    // Prompts ----------------------------------------------------------------------------------------
-    var numCharacters = numCharactersChecker();
-    specialCharacters = confirm("Click OK to include special characters");
-    numericCharacters = confirm("Click OK to include numeric characters");
-    lowerCase = confirm("Click OK to include lowercase characters");
-    upperCase = confirm("Click OK to include uppercase characters");
-    // Prompts ----------------------------------------------------------------------------------------
+  var types = [];
+  var pass = "";
+  // Prompts ---------------------------------------------------------------------------------------------------------
+  var numCharacters = numCharactersChecker();
+  if (confirm("Click OK to include special characters")) {
+    types.push(special);} // If user confrimed special characters, add special array to types array
+  if (confirm("Click OK to include numeric characters")) {
+    types.push(numbers);} // If user confrimed numeric characters, add numbers array to types array
+  if (confirm("Click OK to include lowercase characters")) {
+    types.push(alphabet);} // If user confrimed lowercase characters, add alphabet array to types array
+  if (confirm("Click OK to include uppercase characters")) {
+    types.push(bigAlphabet);} // If user confrimed uppercase characters, add bigAlphabet array to types array
+  // Prompts ---------------------------------------------------------------------------------------------------------
 
-    var pass = "";
-    // For loop adds a random character (based on the user inputs) to "pass", numCharacters amount of times
-    for (var i = 0; i < numCharacters; i++) {
-      var type = Math.ceil(Math.random()*4);
-      pass += addCharacter(type);
-    }
-    return pass;
-}
-
-// Selects appropriate character to return based on user inputs
-function addCharacter(type){
-  if (type === 1) {
-    if (specialCharacters){
-      // If type = 1 and specialCharacters is true, then return a special character
-      return special[Math.floor(Math.random() * special.length)];
-    }
-    else{
-      // Else increment type and start over
-      type += 1;
-      addCharacter(type);
-    }
+  // For loop adds a random character (based on the user's inputs) to "pass", numCharacters amount of times
+  for (var i = 0; i < numCharacters; i++) {
+    var randomType = types[Math.floor(Math.random()*types.length)];
+    pass += randomType[Math.floor(Math.random()*randomType.length)];
   }
-  if (type === 2) {
-    if (numericCharacters){
-      return numbers[Math.floor(Math.random() * numbers.length)];
-    }
-    else{
-      type += 1;
-      addCharacter(type);
-    }
-  }
-  if (type === 3) {
-    if (lowerCase){
-      return alphabet[Math.floor(Math.random() * alphabet.length)];
-    }
-    else{
-      type += 1;
-      addCharacter(type);
-    }
-  }
-  if (type === 4) {
-    if (upperCase){
-      return alphabet[Math.floor(Math.random() * alphabet.length)].toUpperCase();
-    }
-    else{
-      type = 1;
-      return addCharacter(type);
-    }
-  }
-
+  return pass;
 }
 
 // Prompts for number of characters and checks if input is valid
